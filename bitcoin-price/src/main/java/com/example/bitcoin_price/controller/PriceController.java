3c04578917bcb17e0208a97322c775dc7830e683
Manish Doble <manishdoble5@gmail.com>
2025-06-15 20:30:36 +0530

    adding working url and changes

--- bitcoin-price/src/main/java/com/example/bitcoin_price/controller/PriceController.java
+++ bitcoin-price/src/main/java/com/example/bitcoin_price/controller/PriceController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.bitcoin_price.service.BitcoinPriceService;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
+@CrossOrigin(origins = {"http://localhost:5173", "https://localhost:3000"})
 @RestController
 public class PriceController {
 
